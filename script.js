d32218231b1cbd0ac2c1d190b2d0551c552ba843
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-07 16:07:00 +0430

    css update and code refactor

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const leftColor = document.querySelector("#left-color");
 const rightColor = document.querySelector("#right-color");
-const body = document.querySelector("#gradient");
+const body = document.querySelector("body");
 const css = document.querySelector("h3");
 
 css.textContent = "linear-gradient(to right, rgb(27, 80, 27), rgb(102, 109, 3));";
 
-function setGradiant()
-{
+const setGradiant = () => {
     body.style.background = "linear-gradient(to right, "
     + leftColor.value + ", "
     + rightColor.value + ")";
